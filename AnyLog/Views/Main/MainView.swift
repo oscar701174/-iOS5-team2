117a6efe321b71d134d18b8364268ff9f7d778ae
yjoon <yjoon0722@gmail.com>
2025-10-14 13:37:49 +0900

    기본 폴더 구성

---
+++ AnyLog/Views/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  AnyLog
+//
+//  Created by 조영준 on 10/14/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ AnyLog/Views/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  AnyLog
+//
+//  Created by 조영준 on 10/14/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
