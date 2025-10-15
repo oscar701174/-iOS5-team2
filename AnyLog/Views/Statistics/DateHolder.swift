862a2ff0a91b15ce6ff05fb51ef2e6701371b4f5
김태윤 <oscar701174@gmail.com>
2025-10-15 11:07:09 +0900

    Oscar

--- AnyLog/Views/Statistics/DateHolder.swift
+++ AnyLog/Views/Statistics/DateHolder.swift
@@ -6,28 +6,56 @@ class DateHolder: ObservableObject {
     
     @Published var dateSelected: Date = Date()
     
-    var nextMonth: Date {
-        Calendar.current.date(byAdding: .month, value: 1, to: dateSelected)!
-    }
-    var prevMonth: Date {
-        Calendar.current.date(byAdding: .month, value: -1, to: dateSelected)!
-    }
-    
+    var nextMonth: Date { Calendar.current.date(byAdding: .month, value: 1, to: dateSelected) ?? Date() }
+    var prevMonth: Date { Calendar.current.date(byAdding: .month, value: -1, to: dateSelected) ?? Date() }
     
     func nextMonthMove() { dateSelected = nextMonth }
-    
     func prevMonthMove() { dateSelected = prevMonth }
+    
 }
 
 
 extension Date {
+    
+    init(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0) {
+        var comps = DateComponents()
+        comps.year = year
+        comps.month = month
+        comps.day = day
+        comps.hour = hour
+        comps.minute = minute
+        self = Calendar.current.date(from: comps)!
+    }
+    
     var yearMonth: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "yyyy년 M월"
         return formatter.string(from: self)
     }
+    
+    var yearMonthV02: (year: Int, month: Int) {
+        let calendar = Calendar.current
+        return (
+            year: calendar.component(.year, from: self),
+            month: calendar.component(.month, from: self)
+        )
+    }
+    
+    var yearMonthDay: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일"
+        return formatter.string(from: self)
+    }
+    
+    var hour: Int {
+        let calendar = Calendar.current
+        return calendar.component(.hour, from: self)
+    }
+    
+
+    
+    
 }
-
-
 
